cb6cec1ba6e7023e132a7559e7c72a091050929f
Prince Djufo <princedjufo@gmail.com>
2025-06-19 16:02:35 +0100

    modification sur  DetailLabo.scss DetailLabo.jsx

--- src/Pages/DetailLabo.jsx
+++ src/Pages/DetailLabo.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import '../styles/DetailLabo.scss';
 import { images } from '../autres/data';
 import { labos } from '../autres/data';
-
+import { Link } from "react-router-dom";
 const animationClasses = ['fade-up', 'slide-left', 'slide-right', 'zoom-in', 'flip'];
 
 const DetailLabo = () => {
@@ -36,18 +36,33 @@ const DetailLabo = () => {
 
   return (
     <div className="detail-labo-wrapper">
+
+    <div className="header">
+      <div className="logo">
+          <span className="home-logo">
+            
+          <Link to="/laboratoires" className="labo-lien">
+           Nos Laboratoires
+          </Link>
+          </span>
+      </div>
+      <div className="separator" />
+      <div className="title">Instruments Médicaux</div>
+    </div>
       {labos.map((labo, index) => (
+        
         <div
           className={`image-texte-container ${animationClasses[index % animationClasses.length]}`}
           key={index}
           ref={el => (containersRef.current[index] = el)}
         >
+         
           <div className="image-section">
             <img src={images[labo.image]} alt={labo.titre} className="her-img" />
           </div>
           <div className="texte-section">
             <h2>{labo.titre}</h2>
-            <p>{labo.texte}</p>
+            <p className='seul'>{labo.texte}</p>
           </div>
         </div>
       ))}
